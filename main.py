f2ee2241e9de8b40df84eb803141e8081044b499
Luis <Luis>
2025-01-14 19:07:40 -0300

    round

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def tick():
             d_pwm = e_phi / 90
             traccion.set_pwm_and_ratio(50, 0 - d_pwm)
 
-    print (gps.fix,t("g_phi"),round(gps.phi), t("e"),en_campo.etapa, t("Y0_p"),en_campo.Y0_p, t("t_phi"),round(en_campo.target_phi), t("e_phi"),round(e_phi), t("d_pwm"),round(d_pwm), t("x"),round(en_campo.xx), t("y"),round(en_campo.yy))
+    print (gps.fix,t("g_phi"),round(gps.phi), t("e"),en_campo.etapa, t("Y0_p"),round(en_campo.Y0_p), t("t_phi"),round(en_campo.target_phi), t("e_phi"),round(e_phi), t("d_pwm"),round(d_pwm), t("x"),round(en_campo.xx), t("y"),round(en_campo.yy))
 
 import threading, traceback
 
